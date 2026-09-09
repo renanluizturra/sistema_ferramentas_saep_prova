8f55fe1c5da38eac63f49d11f3d3cf35124e7ce9
RENAN LUIZ TURRA <renan_turra@estudante.sesisenai.org.br>
2026-09-09 12:20:06 -0300

    Criacao de get para api e conectando o bd

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,30 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.get('/produtos', (req, res) => {
+    const sql = 'SELECT * FROM PRODUTO';
 
+    connectDB.query(sql, (erro, resultados) => {
+        if (erro) {
+            return res.status(500).json({ erro: 'Erro ao buscar produtos' });
+        }
+        res.json(resultados);
+    });
+});
+server.get('/produtos/ordenados', (req, res) => {
+    const sql = 'SELECT * FROM PRODUTO ORDER BY nome ASC';
+
+    connectDB.query(sql, (erro, resultados) => {
+        if (erro) {
+            return res.status(500).json({ erro: 'Erro ao buscar produtos' });
+        }
+        res.json(resultados);
+    });
+});
+
+
+const PORT = 3025;
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mysql = require ('mysql2')
+
+//Cria a conexão com o banco de dados MySQL com as credenciais de acesso
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'saep_db',
+    port: 3307
+});
+
+//Tenta realizar a conexão com o banco
+connection.connect((erro) => {
+    if (erro) {
+        console.log('Erro ao conectar ao Banco de Dados:', erro);
+        return;
+    }
+    console.log('Banco de Dados saep_db conectado com sucesso!')
+});
+
+//Exporta a variável connection para ser usada no app.js
+module.exports = connection;
