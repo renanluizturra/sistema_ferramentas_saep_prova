e38c69c7d6c3095e43e21743e8ea37e5b398e146
renanluizturra <renanluizturra@gmail.com>
2026-09-16 12:31:12 -0300

    Inserindo rota de Cadastro de produto
    Add endpoint to insert new products into the database with validation for required fields.

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,30 @@ server.get('/produtos/:busca/:nome', (req, res) => {
         res.json(resultados);
     });
 });
+server.post('/produtos', (req, res) => {
+    const {id_categoria, nome, cor, textura, peso, unidade_medida, aplicacao,
+         data_validade, estoque_minimo, estoque_atual, preco_unitario
+        } = req.body;
+
+        if(id_categoria == null || nome == null || cor == null || textura == null || peso == null || unidade_medida == null || aplicacao == null 
+            || data_validade == null || estoque_minimo == null || estoque_atual == null || preco_unitario == null){
+            return res.status(400).json({ erro: 'Todos os campos são obrigatórios' });
+        }
+
+        const sql = 'INSERT INTO PRODUTO (id_categoria, nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    
+    connectDB.query(sql, [id_categoria, nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario], (erro, resultado) => {
+        if (erro) {
+            return res.status(500).json({ erro: 'Erro ao inserir produto' });
+        }
+        res.json({
+            mensagem: 'Produto inserido com sucesso',
+            id: resultado.insertId
+        });
+    });
+});
+
 
 
 const PORT = 3025;
